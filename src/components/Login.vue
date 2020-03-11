e5c35a6bf30782fec7249df954e7a62eee030b5c
java-fancl <fancl3085015@163.com>
2020-03-11 18:23:00 +0800

    项目代码架构提交

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    用户名：<input type="text" v-model="loginForm.username" placeholder="请输入用户名" />
+    <br><br>
+    密码：<input type="password" v-model="loginForm.password" placeholder="请输入密码" />
+    <br><br>
+    <button v-on:click="login">登录</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+          return{
+              loginForm:{
+                  username:'',
+                  password:''
+              },
+              responseResult:[]
+          }
+        }
+    }
+    methods:{
+        login() {
+            this.$axios
+                .post('/login', {
+                    username : this.loginForm.username,
+                    password : this.loginForm.password
+            })
+                .then(successResponse => {
+                    if (successResponse.data().code === 200) {
+                        this.$router.replace({path : '/index'})
+                    }
+                })
+                .catch(failResponse => {})
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
